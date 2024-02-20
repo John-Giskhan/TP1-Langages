b9515b083cd850abcc0d624080bd3130068390d0
qinaught@gmail.com <qinaught@gmail.com>
2024-02-20 18:10:32 -0500

    Implement est_prerequis and simp_pre functions

--- lib/tp1.ml
+++ lib/tp1.ml
@@ -32,11 +32,36 @@ type type_cours = OB | OP | Conc
 
 (* -- À IMPLANTER/COMPLÉTER (10 PTS) ---------------------------------------- *)
 let est_prerequis (lc : cours list) (nc1 : num_cours) (nc2 : num_cours) : int =
-  raise (Non_Implante "est_prerequis non implanté")
+  let prerequis_nc1 = lc_dans_pr (ret_descr lc nc1).pre in
+  if List.exists (fun pre -> pre = nc2) prerequis_nc1 then 1 
+  else 
+    let prerequis_nc2 = lc_dans_pr (ret_descr lc nc2).pre in
+    if List.exists (fun pre -> pre = nc1) prerequis_nc2 then -1 else 0
 
 (* -- À IMPLANTER/COMPLÉTER (30 PTS) ---------------------------------------- *)
+let is_cours_in_list cours lst =
+  List.mem cours.titre lst
+
 let simp_pre (pre : prealables) : prealables =
-  raise (Non_Implante "simp_pre non implanté")
+  let prealables = lc_dans_pr pre in
+  let str = pr2str pre in
+  print_string("before lc_dans_pr");
+  print_newline();
+  print_string str;
+  print_newline();
+  print_string("afteer lc_dans_pr");
+  print_newline();
+
+  List.iter (fun x -> Printf.printf "%s " x) prealables;
+  print_newline();
+
+  let rec filter_pre pre lst = match pre with
+  | CP cours when (is_cours_in_list cours.titre lst) -> Aucun
+  | CCP cours when (is_cours_in_list cours.titre lst) -> Aucun
+  | OU pres -> OU (List.filter_map (fun p -> match filter_pre p lst with Aucun -> None | x -> Some x) pres)
+  | ET pres -> ET (List.filter_map (fun p -> match filter_pre p lst with Aucun -> None | x -> Some x) pres)
+  | _ -> Aucun in 
+  filter_pre pre prealables 
 
 (* -- À IMPLANTER/COMPLÉTER (10 PTS) ---------------------------------------- *)
 let seuls_cours_pgm_dans_pre (lncp : num_cours list) (pre : prealables) :
