ad72a9a881a27769d644f9e248b205b2a0af6f2f
Qi Naught <qinaught@gmail.com>
2024-02-21 16:32:07 -0500

    start impl cours_pgm_par_type

--- lib/tp1.ml
+++ lib/tp1.ml
@@ -33,35 +33,34 @@ type type_cours = OB | OP | Conc
 (* -- À IMPLANTER/COMPLÉTER (10 PTS) ---------------------------------------- *)
 let est_prerequis (lc : cours list) (nc1 : num_cours) (nc2 : num_cours) : int =
   let prerequis_nc1 = lc_dans_pr (ret_descr lc nc1).pre in
-  if List.exists (fun pre -> pre = nc2) prerequis_nc1 then 1 
-  else 
+  if List.exists (fun pre -> pre = nc2) prerequis_nc1 then 1
+  else
     let prerequis_nc2 = lc_dans_pr (ret_descr lc nc2).pre in
     if List.exists (fun pre -> pre = nc1) prerequis_nc2 then -1 else 0
 
 (* -- À IMPLANTER/COMPLÉTER (30 PTS) ---------------------------------------- *)
-let is_cours_in_list cours lst =
-  List.mem cours.titre lst
+let is_cours_in_list cours lst = List.mem cours.titre lst
 
 let simp_pre (pre : prealables) : prealables =
   let prealables = lc_dans_pr pre in
   let str = pr2str pre in
-  print_string("before lc_dans_pr");
-  print_newline();
+  print_string "before lc_dans_pr";
+  print_newline ();
   print_string str;
-  print_newline();
-  print_string("afteer lc_dans_pr");
-  print_newline();
+  print_newline ();
+  print_string "afteer lc_dans_pr";
+  print_newline ();
 
   List.iter (fun x -> Printf.printf "%s " x) prealables;
-  print_newline();
+  print_newline ();
   Aucun
-  (* let rec filter_pre pre lst = match pre with
-  | CP cours when (is_cours_in_list cours.titre lst) -> Aucun
-  | CCP cours when (is_cours_in_list cours.titre lst) -> Aucun
-  | OU pres -> OU (List.filter_map (fun p -> match filter_pre p lst with Aucun -> None | x -> Some x) pres)
-  | ET pres -> ET (List.filter_map (fun p -> match filter_pre p lst with Aucun -> None | x -> Some x) pres)
-  | _ -> Aucun in 
-  filter_pre pre prealables  *)
+(* let rec filter_pre pre lst = match pre with
+   | CP cours when (is_cours_in_list cours.titre lst) -> Aucun
+   | CCP cours when (is_cours_in_list cours.titre lst) -> Aucun
+   | OU pres -> OU (List.filter_map (fun p -> match filter_pre p lst with Aucun -> None | x -> Some x) pres)
+   | ET pres -> ET (List.filter_map (fun p -> match filter_pre p lst with Aucun -> None | x -> Some x) pres)
+   | _ -> Aucun in
+   filter_pre pre prealables *)
 
 (* -- À IMPLANTER/COMPLÉTER (10 PTS) ---------------------------------------- *)
 let seuls_cours_pgm_dans_pre (lncp : num_cours list) (pre : prealables) :
@@ -69,8 +68,30 @@ let seuls_cours_pgm_dans_pre (lncp : num_cours list) (pre : prealables) :
   raise (Non_Implante "seuls_cours_pgm_dans_pre non implanté")
 
 (* -- À IMPLANTER/COMPLÉTER (5 PTS) ----------------------------------------- *)
+let rec process_exigences (cours : num_cours list) = function
+  | [] -> cours
+  | CoursOB (_, num_cours) :: t -> process_exigences (cours @ num_cours) t
+  | PlageCr (_, _, exigences_ext) :: t -> (
+      match exigences_ext with
+      | Cours num_cours_list -> process_exigences (cours @ num_cours_list) t
+      | CoursExclus num_cours_list ->
+          process_exigences (cours @ num_cours_list) t)
+
 let cours_pgm_par_type (pgm : programme) (tc : type_cours) : num_cours list =
-  raise (Non_Implante "cours_pgm_par_type non implanté")
+  let _, _, _, cours_OB, cours_OP, cours_Conc = pgm in
+
+  match tc with
+  | OB ->
+      let _, liste_titre_exigences = cours_OB in
+      let liste_exigences =
+        List.map (fun (_, exigences) -> exigences) liste_titre_exigences
+      in
+      process_exigences [] liste_exigences
+  | _ -> []
+(*
+   | OP -> let (_, liste_exigences) in
+
+   | Conc -> let (_, nb_credits_et_exigences) in *)
 
 (* -- À IMPLANTER/COMPLÉTER (5 PTS) ----------------------------------------- *)
 let cours_pgm (pgm : programme) : num_cours list =
