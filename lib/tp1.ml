1f1f471837bdbc45fac82d85531f7f3c4c7b451e
Qi Naught <qinaught@gmail.com>
2024-02-23 14:27:09 -0500

    Implement simp_pre and seuls_cours_pgm_dans_pre functions

--- lib/tp1.ml
+++ lib/tp1.ml
@@ -39,40 +39,65 @@ let est_prerequis (lc : cours list) (nc1 : num_cours) (nc2 : num_cours) : int =
     if List.exists (fun pre -> pre = nc1) prerequis_nc2 then -1 else 0
 
 (* -- À IMPLANTER/COMPLÉTER (30 PTS) ---------------------------------------- *)
-let is_cours_in_list cours lst = List.mem cours.titre lst
+let cours_est_dans_liste cours liste = List.mem cours.titre liste
 
-let simp_pre (pre : prealables) : prealables =
-  let prealables = lc_dans_pr pre in
-  let str = pr2str pre in
-  (* print_string "before lc_dans_pr";
-     print_newline ();
-     print_string str;
-     print_newline ();
-     print_string "afteer lc_dans_pr";
-     print_newline ();
+(* Applatir OU et ET imbriques *)
+let applatir_OU_ET liste : prealables list =
+  List.fold_left
+    (fun acc pre ->
+      match pre with ET liste | OU liste -> acc @ liste | _ -> acc @ [ pre ])
+    [] liste
 
-     List.iter (fun x -> Printf.printf "%s " x) prealables; *)
-  print_newline ();
-  Aucun
-(* let rec filter_pre pre lst = match pre with
-   | CP cours when (is_cours_in_list cours.titre lst) -> Aucun
-   | CCP cours when (is_cours_in_list cours.titre lst) -> Aucun
-   | OU pres -> OU (List.filter_map (fun p -> match filter_pre p lst with Aucun -> None | x -> Some x) pres)
-   | ET pres -> ET (List.filter_map (fun p -> match filter_pre p lst with Aucun -> None | x -> Some x) pres)
-   | _ -> Aucun in
-   filter_pre pre prealables *)
+let enlever_doublons liste : prealables list = [] ++ liste
+let enlever_aucun liste : prealables list = List.filter (( <> ) Aucun) liste
+
+let rec simp_pre pre =
+  match pre with
+  | OU liste | ET liste -> (
+      let simplifier =
+        liste |> List.map simp_pre |> applatir_OU_ET |> enlever_doublons
+        |> enlever_aucun
+      in
+      match simplifier with
+      | [] -> Aucun
+      | [ x ] -> x
+      | _ -> ( match pre with OU _ -> OU simplifier | _ -> ET simplifier))
+  | _ -> pre
 
 (* -- À IMPLANTER/COMPLÉTER (10 PTS) ---------------------------------------- *)
-let seuls_cours_pgm_dans_pre (lncp : num_cours list) (pre : prealables) :
+let rec seuls_cours_pgm_dans_pre (lncp : num_cours list) (pre : prealables) :
     prealables =
-  raise (Non_Implante "seuls_cours_pgm_dans_pre non implanté")
+  let pre_simplifie = simp_pre pre in
+  match pre_simplifie with
+  | CP cours -> if List.mem cours lncp then pre_simplifie else Aucun
+  | CCP cours -> if List.mem cours lncp then pre_simplifie else Aucun
+  | CRE _ -> pre_simplifie
+  | OU liste | ET liste -> (
+      let process_liste =
+        List.fold_right
+          (fun elem acc ->
+            let filtrer = seuls_cours_pgm_dans_pre lncp elem in
+            match filtrer with Aucun -> acc | _ -> filtrer :: acc)
+          liste []
+      in
+      match process_liste with
+      | [] -> Aucun
+      | [ x ] -> x
+      | _ as filtrer -> (
+          match pre_simplifie with
+          | OU _ -> OU filtrer
+          | ET _ -> ET filtrer
+          | _ -> assert false))
+  | Aucun -> Aucun
 
 (* -- À IMPLANTER/COMPLÉTER (5 PTS) ----------------------------------------- *)
-let rec cours_dans_liste_exigences (exigences : exigences list) (doublons : bool) : num_cours list =
+let rec cours_dans_liste_exigences (exigences : exigences list)
+    (doublons : bool) : num_cours list =
   match exigences with
   | [] -> []
-  | h::t -> if doublons then extrait_lc h @ cours_dans_liste_exigences t doublons
-                        else extrait_lc h ++ cours_dans_liste_exigences t doublons
+  | h :: t ->
+      if doublons then extrait_lc h @ cours_dans_liste_exigences t doublons
+      else extrait_lc h ++ cours_dans_liste_exigences t doublons
 
 let cours_pgm_par_type (pgm : programme) (tc : type_cours) : num_cours list =
   let _, titre, _, cours_OB, cours_OP, cours_Conc = pgm in
@@ -99,7 +124,6 @@ let cours_pgm_par_type (pgm : programme) (tc : type_cours) : num_cours list =
       in
       cours_dans_liste_exigences liste_exigences true
 
-
 (* -- À IMPLANTER/COMPLÉTER (5 PTS) ----------------------------------------- *)
 let cours_pgm (pgm : programme) : num_cours list =
   let _, titre, _, cours_OB, cours_OP, cours_Conc = pgm in
@@ -121,14 +145,15 @@ let cours_pgm (pgm : programme) : num_cours list =
   let (liste_exigences_Conc : exigences list) =
     List.fold_left ( ++ ) [] liste_liste_exigences
   in
-  cours_dans_liste_exigences (liste_exigences_OB ++ liste_exigences_OP ++ liste_exigences_Conc) false
-
-
+  cours_dans_liste_exigences
+    (liste_exigences_OB ++ liste_exigences_OP ++ liste_exigences_Conc)
+    false
 
 (* -- À IMPLANTER/COMPLÉTER (15 PTS) ---------------------------------------- *)
 let cours_contrib_dans_pgm (nc : num_cours) (lpgms : (string * programme) list)
     : (string * type_cours option) list =
-    List.map (fun (dept, programme) -> 
+  List.map
+    (fun (dept, programme) ->
       let cours_OB = cours_pgm_par_type programme OB in
       let cours_OP = cours_pgm_par_type programme OP in
       let cours_Conc = cours_pgm_par_type programme Conc in
@@ -139,36 +164,37 @@ let cours_contrib_dans_pgm (nc : num_cours) (lpgms : (string * programme) list)
         else if List.exists (fun cours -> cours = nc) cours_Conc then Some Conc
         else None
       in
-      (dept, type_cours_option)
-    ) lpgms
-
-      
+      (dept, type_cours_option))
+    lpgms
 
 (* -- À IMPLANTER/COMPLÉTER (25 PTS) ---------------------------------------- *)
-let num_cours_dans_liste_cours (lc : cours list) (lnc : num_cours list):
-    bool = 
-    
-    let num_cours_lc = List.map (fun (num_cours_lc, _) -> num_cours_lc) lc in
-    List.for_all (fun elem -> respecte_motif elem "*-*" && List.mem elem num_cours_lc) lnc
+let num_cours_dans_liste_cours (lc : cours list) (lnc : num_cours list) : bool =
+  let num_cours_lc = List.map (fun (num_cours_lc, _) -> num_cours_lc) lc in
+  List.for_all
+    (fun elem -> respecte_motif elem "*-*" && List.mem elem num_cours_lc)
+    lnc
 
 let regroupe_cours_equiv (lc : cours list) (lnc : num_cours list) :
-
     num_cours list list =
-    if num_cours_dans_liste_cours lc lnc then (
-      let equiv = nc_eq lc in
-      let cours_equiv = List.fold_left (fun acc num_cours ->
-        let groupes_equiv, groupes_non_equiv = List.partition (fun groupe -> List.exists (equiv num_cours) groupe) acc in
-        match groupes_equiv with
-        | [] -> [num_cours] :: acc
-        | [x] -> (num_cours :: x) :: groupes_non_equiv
-        | _ -> failwith "Erreur, plusieurs groupes sont equivalents."
-      ) [] lnc in
+  if num_cours_dans_liste_cours lc lnc then
+    let equiv = nc_eq lc in
+    let cours_equiv =
+      List.fold_left
+        (fun acc num_cours ->
+          let groupes_equiv, groupes_non_equiv =
+            List.partition
+              (fun groupe -> List.exists (equiv num_cours) groupe)
+              acc
+          in
+          match groupes_equiv with
+          | [] -> [ num_cours ] :: acc
+          | [ x ] -> (num_cours :: x) :: groupes_non_equiv
+          | _ -> failwith "Erreur, plusieurs groupes sont equivalents.")
+        [] lnc
+    in
 
-        let groupes_tries = List.map (List.sort Stdlib.compare) cours_equiv in
-        List.sort Stdlib.compare groupes_tries
-    )
-    else
-      failwith "un des cours présent dans [lnc] n'est 
-      pas défini dans [lc]"
-
-
+    let groupes_tries = List.map (List.sort Stdlib.compare) cours_equiv in
+    List.sort Stdlib.compare groupes_tries
+  else
+    failwith
+      "un des cours présent dans [lnc] n'est \n      pas défini dans [lc]"
